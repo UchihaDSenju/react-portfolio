45e4d6f5164156983c1613183626a8cf78d7ca0c
Tariq <rafiqtariq19@gmail.com>
2023-04-01 23:42:45 +0530

    navbar done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import {FaBars} from 'react-icons/fa'
 
 function Navbar() {
+
+  const bars = useRef(null);
+  const navPane = useRef(null);
+
+  const barClick = () => {
+    navPane.current.classList.toggle('show');
+  }
+
+
   return (
-    <div>Navbar</div>
+    <>
+        <nav className='nav-bar'>
+          <button onClick={barClick} className='bars'>
+              <FaBars />
+          </button>
+
+          <ul ref={navPane}>
+              <li><a href=''>Home</a ></li>
+              <li><a href=''>Projects</a ></li>
+              <li><a href=''>Experience</a ></li>
+          </ul>
+        </nav>
+    </>
   )
 }
 
